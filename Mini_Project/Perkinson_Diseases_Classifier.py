39491d451d15a1c1243bb6d984bb427265b75ee5
PrathameshDeshpande <60612588+PrathameshDeshpande@users.noreply.github.com>
2020-09-03 11:59:37 +0530

    changes made in decoding of data

--- Mini_Project/Perkinson_Diseases_Classifier.py
+++ Mini_Project/Perkinson_Diseases_Classifier.py
@@ -1,3 +1,13 @@
 import pandas as pd
 import csv
+from sklearn.model_selection import train_test_split
+
 df = pd.read_csv("C:/Users/91797/Desktop/ML_Intern_Technocolab/Mini_Project/data.csv")
+x = df.drop(["name","status"],axis=1)
+y = df["status"]
+y = pd.DataFrame(y)
+x_train, x_test, y_train, y_test = train_test_split(x, y,
+                                                    stratify=y,
+                                                    test_size=0.20)
+
+
