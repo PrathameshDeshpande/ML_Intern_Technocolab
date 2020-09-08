359266c212af1a0e72038e8dab87660183824110
PrathameshDeshpande <60612588+PrathameshDeshpande@users.noreply.github.com>
2020-09-08 23:33:13 +0530

    Classifier Added

--- Mini_Project/Perkinson_Diseases_Classifier.py
+++ Mini_Project/Perkinson_Diseases_Classifier.py
@@ -1,7 +1,8 @@
 import pandas as pd
-import xgboost
+from xgboost import XGBClassifier
 import csv
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 df = pd.read_csv("C:/Users/91797/Desktop/ML_Intern_Technocolab/Mini_Project/data.csv")
 x = df.drop(["name","status"],axis=1)
@@ -10,4 +11,10 @@ y = pd.DataFrame(y)
 x_train, x_test, y_train, y_test = train_test_split(x, y,
                                                     stratify=y,
                                                     test_size=0.20)
-
+XGBC = XGBClassifier()
+XGBC.fit(x_train,y_train)
+print(XGBC)
+y_pred = XGBC.predict(x_test)
+y_pred = pd.DataFrame(y_pred)
+accuracy = accuracy_score(y_test, y_pred)
+print(accuracy)
